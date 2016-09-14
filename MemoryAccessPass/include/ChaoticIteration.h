e59d0b14c330fc82c84c8988ed59ce7e21fc8b9a
Omer Anson <oaanson@gmail.com>
2016-09-14 20:18:39 +0300

    Chaotic iteration should iterate basic blocks by order in function

--- MemoryAccessPass/include/ChaoticIteration.h
+++ MemoryAccessPass/include/ChaoticIteration.h
@@ -1,6 +1,8 @@
 #ifndef CHAOTIC_ITERATION_H
 #define CHAOTIC_ITERATION_H
 
+#include <list>
+
 #include <llvm/InstVisitor.h>
 #include <llvm/Support/raw_ostream.h>
 
@@ -11,20 +13,47 @@ namespace MemoryAccessPass {
 		bool join(const llvm::BasicBlock * from, const llvm::BasicBlock * to);
 	};
 
+	class BasicBlockInFunctionComparator {
+	private:
+		llvm::Function & m_function;
+	public:
+		BasicBlockInFunctionComparator(llvm::Function & function): m_function(function) {}
+		bool operator()(llvm::BasicBlock * left, llvm::BasicBlock * right) const {
+			if (left == right) {
+				return false;
+			}
+			int left_idx = -1;
+			int right_idx = -1;
+			int idx = 0;
+			for (llvm::Function::const_iterator it = m_function.begin(),
+								ie = m_function.end();
+					it != ie; it++) {
+				if (left == it) {
+					left_idx = idx;
+				}
+				if (right == it) {
+					right_idx = idx;
+				}
+				idx++;
+			}
+			return (left_idx < right_idx);
+		}
+	};
+
 	template <class T> class ChaoticIteration {
 	private:
 		T & m_visitor;
 	protected:
 		void populateWorklistWithSuccessors(
-				std::vector<llvm::BasicBlock *> & worklist,
+				std::list<llvm::BasicBlock *> & worklist,
 				const llvm::BasicBlock & element) {
 			const llvm::TerminatorInst * terminator = element.getTerminator();
 			int successorCount = terminator->getNumSuccessors();
 			T & visitor = getVisitor();
 			for (int idx = 0; idx < successorCount; idx++) {
 				llvm::BasicBlock * BB = terminator->getSuccessor(idx);
-				llvm::errs() << "Adding basic block to worklist: " << BB->getName() << "\n";
 				if (visitor.join(&element, BB)) {
+					llvm::errs() << "Adding to worklist: " << BB->getName() << ": " << element.getName() << " -> " << BB->getName() << "\n";
 					worklist.push_back(BB);
 				}
 			}
@@ -40,17 +69,20 @@ namespace MemoryAccessPass {
 		}
 		void iterate(llvm::BasicBlock * BB) { return iterate(*BB); }
 		void iterate(llvm::BasicBlock & BB) {
-			std::vector<llvm::BasicBlock *> worklist;
+			BasicBlockInFunctionComparator comparator(*BB.getParent());
+			std::list<llvm::BasicBlock *> worklist;
+			llvm::errs() << "Adding to worklist: " << BB.getName() << "\n";
 			worklist.push_back(&BB);
 			T & visitor = getVisitor();
 			while (!worklist.empty()) {
-				std::vector<llvm::BasicBlock *>::iterator
+				std::list<llvm::BasicBlock *>::iterator
 						first_element = worklist.begin();
+				worklist.pop_front();
 				llvm::BasicBlock * element = *first_element;
-				llvm::errs() << "Chaotic iteration on basic block: " << element->getName() << "\n";
+				llvm::errs() << "Working on: " << element->getName() << "\n";
 				visitor.visit(element);
-				worklist.erase(first_element);
 				populateWorklistWithSuccessors(worklist, *element);
+				worklist.sort(comparator);
 			}
 		}
 	};
